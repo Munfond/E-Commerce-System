0e8011a06169acbdadd0d9e96dad1368aa047e1b
trsilwi02 <anh.td235009@sis.hust.edu.vn>
2026-05-15 13:32:59 +0700

    update src/app.js to deploy port

--- src/app.js
+++ src/app.js
@@ -38,3 +38,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is barking on port ${PORT}`);
 });
+
+// app.listen(PORT, () => {
+//     console.log(`Server is barking on port ${PORT}`);
+// });
