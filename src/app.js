15886184223760aa32aae59aa97a95ec351896f4
trsilwi02 <anh.td235009@sis.hust.edu.vn>
2026-05-15 13:30:24 +0700

    update app.js (update port)

--- src/app.js
+++ src/app.js
@@ -35,6 +35,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is barking on port http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is barking on port ${PORT}`);
 });
