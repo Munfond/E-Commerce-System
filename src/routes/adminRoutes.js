7049a1d52cc1d11f4a8c56da9873e536669704f5
30sweetener09 <vuongan3467@gmail.com>
2026-06-09 23:08:18 +0700

    adjust: Admin user status

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -12,6 +12,5 @@ router.patch('/shops/:id/control', adminController.controlShop);
 router.get('/shops', adminController.getShops);
 router.get('/products', adminController.getProducts);
 router.get('/users', adminController.getAllUsers);
-router.post('/users/:id/status', adminController.changeUserStatus);
-
+router.patch('/users/:id/status', adminController.changeUserStatus);
 module.exports = router;
